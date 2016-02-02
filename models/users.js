b4f5608a9ba6d61d2302b25d0ddeaf7072abb864
domsmith94 <domsmith94@gmail.com>
2016-02-02 14:03:50 +0000

    model change

--- models/users.js
+++ models/users.js
@@ -17,7 +17,7 @@ mongoose.connect(uristring, function(err, res){
 }); //used in local development
 
 var UserSchema = new Schema({
-	email: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
 	firstname: String,
 	lastname: String,
 	mobile:String,
@@ -48,6 +48,13 @@ UserSchema.pre('save', function(next) {
 	});
 });
 
+// Check password is valid
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 
 module.exports = mongoose.model('User', UserSchema);
