53e9a9bb6e5411a8daa45175f2ff66f3ea8e3fef
domsmith94 <domsmith94@gmail.com>
2016-03-15 12:02:32 +0000

    [view] and [sms] comments

--- utility/sms.js
+++ utility/sms.js
@@ -2,15 +2,24 @@ var Ask = require('../models/ask');
 var User = require('../models/users');
 var SMS = require('../models/sms');
 var auth = require('../config/auth');
-
 var privateConfig = require('../config/private');
 var accountSid = process.env.accountSid || privateConfig.accountSid;
 var authToken = process.env.authToken || privateConfig.authToken;
 var twilioAPI = require("twilio")
 var twilio = twilioAPI(accountSid, authToken);
 
+// This module is used to perform all the SMS functionality of my application. This includes both sending
+// and receiving SMS. This is the only part of the application where twilio library is used. When CanYouPlay
+// is running on Heroku environment, the relevant tokens are stored as environment variables. When running 
+// locally these values are read from /config/private.js. 
 module.exports = {
 
+	// This is triggered when a person is Asked to play in a fixture and the player has chosen to receive
+	// SMS notifications. The ObjectId of the Ask object in the mongo store is passed as parameter and retrieved.
+	// We then check to see if any SMS objects for the players number exist in mongo (Only 1 number = 1 SMS object)
+	// This is because the system can only have 1 outstanding SMS request for a player at a time due to the inability
+	// to know which SMS request a player is responding to if there is more than 1. Old one is replaced if exists or
+	// new one created. Then message is constructed and sent out. 
 	sendSMS : function(askId) {
 		Ask.findOne({_id: askId}).
 			populate('fixture').
@@ -21,13 +30,14 @@ module.exports = {
 						console.log(err)
 						console.log('Problem accessing SMS collection for number ' + ask.player.mobile);
 					} else if (!sms) {
-						// Create a new SMS object
+						console.log('No SMS object found for ' + ask.player.mobile + '. Creating new one.');
 						sms = new SMS();
 					}
 
 					sms.ask = ask._id;
 					sms.mobile = ask.player.mobile;
 
+					console.log('Constructing SMS message to send for Ask object '+ ask._id);
 					var message = 'Hi ' + ask.player.firstname + '. You have been asked to play for ' + 
 					ask.fixture.side + ' against ' + ask.fixture.opposition + ' on ' +
 					ask.fixture.date + '. If you can play reply YES, if you can\'t reply NO. Thanks'
@@ -37,15 +47,22 @@ module.exports = {
 						to: ask.player.mobile,
 						from: "447481345982"
 					}, function(err, message) {
-						console.log(message.sid);
+						console.log('Sent SMS with Twilio SID - ' + message.sid);
 						sms.save();
 					});
-
 				});
-
 			});
-
 	},
+
+	// This is the final destination of what happens when a user replies to the twilio number. twilio
+	// has been set up to forward the message (and it's meta data) to the URL 
+	// http://canyouplay.herokuapp.com/app/sms-reply/, which then calls receivedSMS function, passing in the
+	// message data. SMS object is found by the number that has sent message from mongo. Parse message and then
+	// change the values of the Ask object to reflect the users response. Additional work is then done to update
+	// a users availability and automatically reject any other Ask requests for the same date. Returns a response
+	// 
+	// TODO: Most of the code here is taken from api.js router.put('/ask') route (which is also used to reply 
+	// to Ask requests). This code needs to be more modular and needs to be re-factored.
 
 	receivedSMS : function(body) {
 		SMS.findOne({mobile: body.From}, function(err, sms){
@@ -72,10 +89,8 @@ module.exports = {
 						if (err) {
 							console.log(err);
 						} else {
-
 							// Now we must add the date of fixture to a date that is unavailable for person
 							// as they are playing
-
 							User.findOneAndUpdate(
 								{_id: ask.player},
 								{$addToSet: {not_avail_on: ask.fixture.date}}, 
@@ -98,24 +113,15 @@ module.exports = {
 									asks[i].save();
 								}
 							});
-
-
 						}
 					});
 				}
-
 			});
 
-			
 			sms.remove();
 			var resp = twilioAPI.TwimlResponse();
 			return resp.toString();
 
 		});
-
 	},
-
 }
-
-
-
